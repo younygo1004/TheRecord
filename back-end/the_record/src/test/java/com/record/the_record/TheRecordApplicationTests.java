7b7c604f83f73a4e4f3f52662c53ba662bfb72ee
조용구 <younygo1004@gmail.com>
2022-04-21 15:37:52 +0900

    feat : BackEnd 기본 의존성 추가

---
+++ back-end/the_record/src/test/java/com/record/the_record/TheRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.record.the_record;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
