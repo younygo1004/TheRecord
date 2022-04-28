644ae6ce2823860b8bca1f2b4f6cc551b391ac13
조용구 <younygo1004@gmail.com>
2022-04-28 14:54:56 +0900

    fix : Jenkinsfile 재생성

---
+++ Jenkinsfile/Jenkinsfile.groovy
@@ -0,0 +1,53 @@
+pipeline {
+
+    echo 'start'
+	agent none
+	options { skipDefaultCheckout(false) }
+	stages {
+		stage('git pull') {
+			agent any
+			steps {
+                echo 'git pulling...'
+				checkout scm
+			}
+		}
+
+		stage('Docker build') {
+			agent any
+			steps {
+                echo 'docker build...'
+				sh 'docker build -t backend:latest /var/jenkins_home/workspace/record/back-end/the_record'
+                sh 'docker build -t frontend:latest /var/jenkins_home/workspace/record/front-end/the-record' 
+			}
+		}
+        
+		stage('Docker run') {
+			agent any
+			steps {
+                echo 'docker run...'
+				sh 'docker ps -f name=backend -q \
+		        | xargs --no-run-if-empty docker container stop'
+                sh 'docker ps -f name=frontend -q | xargs --no-run-if-empty docker container stop'
+
+				sh 'docker container ls -a -f name=backend -q \
+		        | xargs -r docker container rm'
+                sh 'docker container ls -a -f name=frontend -q | xargs -r docker container rm'
+
+				sh 'docker images -f dangling=true && docker rmi $(docker images -f dangling=true -q)' 	
+                
+                sh 'docker run -d --name frontend \
+						-p 80:80 \
+						-p 443:443 \
+						-v /etc/localtime:/etc/localtime:ro \
+						--network recordnetwork \
+						frontend:latest'
+
+
+				sh 'docker run -itd -v /home/ubuntu/deploy/data/record:/home/ubuntu/deploy/data/record -p 8080:8080 --name backend backend'
+
+
+            }
+		}
+		
+	}
+}
