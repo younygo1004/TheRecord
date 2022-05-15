624e95ce40938a06b737c87e461ca1789e230998
오은진 <oeunj08@naver.com>
2022-05-15 18:22:20 +0900

    fix     : OPENVIDU 비밀번호 변경

--- front-end/the-record/src/pages/Photo/PhotoBooth.js
+++ front-end/the-record/src/pages/Photo/PhotoBooth.js
@@ -20,10 +20,10 @@ import '../../styles/photo/photobooth.css';
 import UserVideoComponent from '../../components/Album/UserVideoComponent';
 
 const OPENVIDU_SERVER_URL = 'https://the-record.co.kr:4443';
-const OPENVIDU_SERVER_SECRET = process.env.REACT_APP_SERVER_SECRET;
+// const OPENVIDU_SERVER_SECRET = process.env.REACT_APP_SERVER_SECRET;
 const { REACT_APP_REMOVEBG_API_TOKEN } = process.env;
 // const OPENVIDU_SERVER_URL = `https://${window.location.hostname}:4443`;
-// const OPENVIDU_SERVER_SECRET = 'MY_SECRET';
+const OPENVIDU_SERVER_SECRET = 'MY_SECRET';
 
 class PhotoBooth extends Component {
   constructor(props) {
