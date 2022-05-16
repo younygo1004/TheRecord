3aeb81ecd68d39d63e264d2f164ed85c0e2d0667
leeejuhyeong <wnguddl827@gmail.com>
2022-05-16 18:11:26 +0900

    fix : cors 설정

--- back-end/the_record/src/main/java/com/record/the_record/security/WebSecurityConfig.java
+++ back-end/the_record/src/main/java/com/record/the_record/security/WebSecurityConfig.java
@@ -15,6 +15,10 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -65,11 +69,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("https://the-record.co.kr");
-        configuration.addAllowedOrigin("http://localhost:3000");
-        configuration.addAllowedMethod("*");
+        configuration.setAllowedOrigins(Arrays.asList("https://the-record.co.kr", "http://localhost:3000"));
+        configuration.setAllowedMethods(Collections.singletonList("*"));
         configuration.setAllowCredentials(true);
-        configuration.addAllowedHeader("X-AUTH-TOKEN");
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
